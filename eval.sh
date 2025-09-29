bd6840486f0dc2fddc34f5927832a626790cb3a2
James <jlbaker361@gmail.com>
2025-09-29 18:52:54 -0400

    created eval

---
+++ eval.sh
@@ -0,0 +1,2 @@
+sbatch -J free --err=slurm_chip/eval/mtg.err --out=slurm_chip/eval/mtg.out runpygpu_chip.sh evaluation.py --src_dataset jlbaker361/mtg --dest_dataset jlbaker361/free-mtg
+sbatch -J free --err=slurm_chip/eval/dreambooth.err --out=slurm_chip/eval/dreambooth.out runpygpu_chip.sh evaluation.py --src_dataset jlbaker361/dreambooth --dest_dataset jlbaker361/free-dreambooth
