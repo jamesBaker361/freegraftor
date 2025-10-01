a72cb778e4e0503d53d50d6dc913cf6ceeb0a533
James <jlbaker361@gmail.com>
2025-10-01 13:51:56 -0400

    nit

--- evaluation.py
+++ evaluation.py
@@ -166,10 +166,10 @@ def main(args):
         output_dict["text_score"].append(text_score)
         output_dict["prompt"].append(prompt)
 
-        accelerator.print(dino_score)
+        '''accelerator.print(dino_score)
 
         for k,v in output_dict.items():
-            accelerator.print(k, type(v[0]))
+            accelerator.print(k, type(v[0]))'''
 
 
     accelerator.log({
